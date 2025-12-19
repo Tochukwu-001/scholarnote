3283182bfadae63fe76025c97a70c49fce8da572
zane <zaneamune13@gmail.com>
2025-12-19 17:20:14 +0000

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <main className='border-t border-gray-200 md:px-20 md:py-4 p-3'>
+       <Link href={"/"} className='flex items-center gap-1'>
+            <Image src={"/logo.png"} alt='logo' width={500} height={500} className='w-10 h-10 z-50'/>
+            <p>Scholarnote</p>
+            </Link>
+          <div></div>
+    </main>
+
   )
 }
 
