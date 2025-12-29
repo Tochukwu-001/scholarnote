3c803e71f84127a8b78cc221792f242cb5bcbd59
Dimazoro Chinonso <chinonsomarcelino81@gmail.com>
2025-12-29 13:28:44 +0100

    initial commit

---
+++ app/auth/signin/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='min-h-dvh grid md:grid-cols-2'>
+      <div>
+        <h1>
+            Create an account or Sign in with an option below
+        </h1>
+      </div>
+      <div className="min-h-dvh bg-[url('/bg1.jpg')]"></div>
+    </main>
+  )
+}
+
+export default page
---
+++ components/Navbar.jsx
@@ -0,0 +1,75 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { FiUser } from "react-icons/fi"
+import { HiMenuAlt3 } from "react-icons/hi"
+import { IoMdClose } from "react-icons/io"
+
+
+const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+    
+
+    const navLinks = [
+        {
+            label: "Home",
+            url: "/",
+        },
+        {
+            label: "About",
+            url: "/about",
+        },
+        {
+            label: "Contact",
+            url: "/contact",
+        },
+        {
+            label: "Library",
+            url: "/resources",
+        },
+
+    ]
+    return (
+        <section className='sticky'>
+            <nav className='flex items-center justify-center md:px-10 p-3 py-2 shadow-md relative top-0 bg-white'>
+            <Link href={"/"} className='flex items-center gap-1'>
+                <Image src={"/logo.png"} alt='logo' width={500} height={500} className='w-10 h-10 z-50' />
+                <p>Scholarnote</p>
+            </Link>
+
+            <div className='ml-auto flex items-center gap-10 max-md:hidden'>
+                {
+                    navLinks.map((item, index) => (
+                        <Link key={index} href={item.url} className='hover:text-orange-600 transition-all 
+                        duration-200'>{item.label}</Link>
+                    ))
+                }
+            </div>
+            <Link href={"#"} className='ml-10 border px-4 py-1 flex items-center gap-1 
+            hover:text-orange-600 transition-all duration-200 max-md:ml-auto z-50'><FiUser />
+                <p className='max-md:hidden'>Sign In</p>
+            </Link>
+
+            {/* mobile and tablet navbar */}
+            <div className={`h-dvh bg-white lg:hidden absolute top-0 left-0 w-full
+                 flex-col items-center gap-10 pt-10 ${open ? "flex" : "hidden"}`}>
+                {
+                    navLinks.map((item, i)=> (
+                        <Link key={i} href={item.url}>{item.label}</Link>
+                    ))
+                }
+            </div>
+
+            <button onClick={() => { setOpen(!open) }} className='md:hidden ml-3 text-xl z-50'>
+                {
+                    open ? <IoMdClose /> : <HiMenuAlt3 />
+                }
+            </button>
+        </nav>
+        </section>
+    )
+}
+
+export default Navbar
