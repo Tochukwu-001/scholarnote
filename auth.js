0727749d3c0bb60c2dbcd9580694a95234c4ee40
nimiye-cmyk <ibiamanimiye@gmail.com>
2026-01-26 13:57:14 +0000

    updates

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import { cert } from "firebase-admin/app";
+// import { FirestoreAdapter } from "@auth/firebase-adapter";
+// import { cert } from "firebase-admin/app";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
@@ -11,11 +11,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
 
-  adapter: FirestoreAdapter({
-    credential: cert({
-      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
-    }),
-  }),
+  // adapter: FirestoreAdapter({
+  //   credential: cert({
+  //     projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+  //     clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+  //     privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
+  //   }),
+  // }),
 });
