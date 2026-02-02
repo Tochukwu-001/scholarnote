31306bc9c19d6856583bf61787db45a29b40699a
Dimazoro Chinonso <chinonsomarcelino81@gmail.com>
2026-02-02 13:01:06 +0100

    update

--- app/resources/[id]/page.jsx
+++ app/resources/[id]/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { doc, getDoc } from "firebase/firestore";
 import { db } from '@/config/firebase';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa6';
 
 
 const page = async () => {
@@ -10,7 +12,7 @@ const page = async () => {
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
+            ret
         } else {
             // docSnap.data() will be undefined in this case
             console.log("No such document!");
@@ -20,9 +22,22 @@ const page = async () => {
 
     fetchSinglePost()
     return (
-        <div>
-            dynamic route
-        </div>
+        <main className='min-h-dvh'>
+            <Link href={"/resources"} className=''><FaArrowLeft /></Link>
+            <section className='w-1/2 mx-auto my-10'>
+                <h1 className='text-center text-2xl font-bold text-gray-900'>{post.title}</h1>
+                <span className='flex items-center gap-1 justify-center'>
+                    <p>by</p>
+                    <p className='font-semibold'>{post.author}</p>
+                </span>
+                <p className='bg-yellow-200 w-fit mx-auto px-4 py-1 rounded-full text-gray-800'>{post.category}</p>
+                <p className='text-lg'>{post.note}</p>
+                <span className='flex items-center gap-1 text-xs'>
+                    <p>Posted on</p>
+                    <p>{post.timestamp}</p>
+                </span>
+            </section>
+        </main>
     )
 }
 
